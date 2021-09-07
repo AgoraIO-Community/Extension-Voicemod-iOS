f05d9eea96579284e29edbc63249ae704a215708
Max Cobb <maxxc@mac.com>
2021-09-07 11:04:27 +0100

    first commit

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "VoicemodAudioFilterExtension",
+    defaultLocalization: "en",
+    platforms: [.iOS(.v8)],
+    products: [
+        .library(
+            name: "VoicemodAudioFilterExtension",
+            targets: ["VoicemodAudioFilterExtension"]
+        )
+    ],
+    dependencies: [],
+    targets: [
+        .binaryTarget(
+            name: "VoicemodAudioFilterExtension",
+            url: "https://github.com/maxxfrazer/Voicemod-swiftpm/releases/download/0.0.1/VoicemodAudioFilterExtension.xcframework.zip",
+            checksum: "75a2285b03f909b2ce8dbf6c22dc30e53b14024e61f10798fe2bc68d19149dc2"
+        )
+    ]
+)
---
+++ README.md
@@ -0,0 +1,26 @@
+# Voicemod's Audio Filter Extension for Agora
+
+<p align="center">
+  <img src="https://github.com/maxxfrazer/Voicemod-swiftpm/actions/workflows/swiftpm-resolve.yml/badge.svg"/>
+</p>
+
+
+Install Agora Real-time Messaging SDK easily with Swift Package Manager.
+
+![](media/swiftpm-agora.png)
+
+[Click here for full documentation](https://console-preprod-2.agora.io/marketplace/extension/introduce?serviceName=voicemod).
+
+## Installation
+
+Add the URL of this repository to your Xcode 11+ Project.
+
+Go to _File > Swift Packages > Add Package Dependency_, and paste in the link to this repository:
+
+`https://github.com/maxxfrazer/Voicemod-swiftpm.git`
+
+---
+
+If you have issues installing this Swift Package:
+
+In Xcode's File menu, select 'Swift Packages' and then 'Reset Package Caches'.
