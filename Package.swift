e5035f8fbd83c50a5d2823b7e7c096d6210373cd
Max Cobb <maxxc@mac.com>
2022-06-08 09:44:07 +0100

    bump to 0.2.1

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "VoicemodAudioFilterExtension",
     defaultLocalization: "en",
-    platforms: [.iOS(.v8)],
+    platforms: [.iOS(.v9)],
     products: [
         .library(
             name: "VoicemodAudioFilterExtension",
@@ -17,8 +17,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "VoicemodAudioFilterExtension",
-            url: "https://github.com/AgoraIO-Community/Extension-Voicemod-iOS/releases/download/0.1.2/VoicemodAudioFilterExtension.xcframework.zip",
-            checksum: "75a2285b03f909b2ce8dbf6c22dc30e53b14024e61f10798fe2bc68d19149dc2"
+            url: "https://github.com/AgoraIO-Community/Extension-Voicemod-iOS/releases/download/0.2.1/VoicemodAudioFilterExtension.xcframework.zip",
+            checksum: "c9de2778305ce118621b23841ecb19141508ff2731fb02651549df4ab27e2376"
         )
     ]
 )
